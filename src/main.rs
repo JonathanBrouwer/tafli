b5444cbc69636072c95d6db7bbf05705a48e1ea6
JonathanBrouwer <jonathantbrouwer@gmail.com>
2021-01-10 13:19:46 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use actix_cors::Cors;
+use actix_web::{http::header, middleware::Logger, App, HttpServer};
+
+mod user;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix=info");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin("http://localhost:8080")
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .supports_credentials()
+                    .max_age(3600),
+            )
+            .wrap(Logger::default())
+            .service(user::info)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
+}
---
+++ tafli/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container"><a href="/" class="navbar-brand mr-3"><span class="logo">Litafl</span></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div id="navbarSupportedContent" class="navbar-collapse collapse" style="">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item"><a href="/" class="nav-link">Play</a></li>
+          <li class="nav-item"><a href="/learn" class="nav-link">Learn</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: "hello"
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  height: 100%;
+  background: #f0f0f0;
+}
+</style>
