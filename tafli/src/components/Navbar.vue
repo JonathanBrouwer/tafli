536a4acd85d83df25b8c6803a0344cbe87d7ad00
JonathanBrouwer <jonathantbrouwer@gmail.com>
2021-01-10 18:07:17 +0100

    Tafli

--- tafli/src/components/Navbar.vue
+++ tafli/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <!-- Navbar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-white mb-4" style="box-shadow: 0 0 10px rgba(0,0,0,.05);">
     <div class="container">
-      <a href="/" class="navbar-brand">Litafl</a>
+      <a href="/" class="navbar-brand">Tafli</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
