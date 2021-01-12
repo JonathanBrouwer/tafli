fbfe70b40088e8d5d895cb8efff3e08580047410
JonathanBrouwer <jonathantbrouwer@gmail.com>
2021-01-12 11:41:10 +0100

    Fix that you can capture the king

--- src/tafl/rules/capture_rule.rs
+++ src/tafl/rules/capture_rule.rs
@@ -1,10 +1,10 @@
 use crate::tafl::board::BoardConfiguration;
 use crate::tafl::rules::rule::Rule;
-use crate::tafl::board::FieldState::Empty;
+use crate::tafl::board::FieldState::{Empty, WhiteKing};
 
 pub struct CaptureRule;
 impl Rule for CaptureRule {
-    fn make_move(&self, board: &mut BoardConfiguration, from: (usize, usize), to: (usize, usize)) {
+    fn make_move(&self, board: &mut BoardConfiguration, _from: (usize, usize), to: (usize, usize)) {
         //Check for direct captures
         [(1, 0), (-1, 0), (0, 1), (0, -1)].iter().for_each(|dir| {
             let p1 = ((to.0 as isize + dir.0), (to.1 as isize + dir.1));
@@ -16,8 +16,9 @@ impl Rule for CaptureRule {
                 let p2 = (p2.0 as usize, p2.1 as usize);
                 //If both squares are not empty
                 if board.fields[p1.0][p1.1] != Empty && (BoardConfiguration::special_squares().contains(&p2) || board.fields[p2.0][p2.1] != Empty) {
-                    //If the piece at p1 is enemy, and the piece at p2 is friendly
-                    if board.fields[p1.0][p1.1].player().unwrap() != board.turn && (BoardConfiguration::special_squares().contains(&p2) || board.fields[p2.0][p2.1].player().unwrap() == board.turn) {
+                    //If the piece at p1 is enemy and not the king, and the piece at p2 is friendly
+                    if board.fields[p1.0][p1.1].player().unwrap() != board.turn && board.fields[p1.0][p1.1] != WhiteKing &&
+                        (BoardConfiguration::special_squares().contains(&p2) || board.fields[p2.0][p2.1].player().unwrap() == board.turn) {
                         //Capture p1
                         board.fields[p1.0][p1.1] = Empty;
                     }
@@ -34,4 +35,30 @@ fn test_capture() {
     assert_eq!(Ok(()), board.make_move((7, 5), (7, 6)));
     assert_eq!(Ok(()), board.make_move((3, 10), (3, 6)));
     assert_eq!(Empty, board.fields[3][5]);
+}
+
+#[test]
+fn test_not_capture_king() {
+    let mut board = BoardConfiguration::new();
+    let moves = [
+        ((0, 3), (0, 1)),
+        ((4,4), (1,4)),
+        ((5,1), (1,1)),
+        ((5,3), (5,1)),
+        ((0,1), (0,3)),
+        ((6,4), (6,1)),
+        ((7,0), (7,2)),
+        ((5,4), (9,4)),
+        ((7,2), (6,2)),
+        ((5,5), (5,4)),
+        ((6,2), (6,4)),
+        ((7,5), (7,4)),
+        ((4,0), (4,4)),
+        ((5,1), (4,1)),
+        ((6,0), (6,4))
+    ];
+    for mv in moves.iter() {
+        assert_eq!(Ok(()), board.make_move(mv.0, mv.1));
+    }
+    assert_eq!(WhiteKing, board.fields[5][4]);
 }
