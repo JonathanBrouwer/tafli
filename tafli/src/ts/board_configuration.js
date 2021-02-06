b67a7ce2c38c7c02e81117c33e9cc1565abf60bb
JonathanBrouwer <jonathantbrouwer@gmail.com>
2021-02-06 14:49:47 +0100

    Player names working

---
+++ tafli/src/ts/board_configuration.js
@@ -0,0 +1,41 @@
+"use strict";
+exports.__esModule = true;
+exports.BoardSide = exports.Player = exports.player = exports.FieldState = exports.BoardConfiguration = void 0;
+var BoardConfiguration = /** @class */ (function () {
+    function BoardConfiguration(fields, turn) {
+        this.fields = fields;
+        this.turn = turn;
+    }
+    return BoardConfiguration;
+}());
+exports.BoardConfiguration = BoardConfiguration;
+var FieldState;
+(function (FieldState) {
+    FieldState["WhiteKing"] = "WhiteKing";
+    FieldState["WhitePiece"] = "WhitePiece";
+    FieldState["BlackPiece"] = "BlackPiece";
+    FieldState["Empty"] = "Empty";
+})(FieldState = exports.FieldState || (exports.FieldState = {}));
+function player(state) {
+    if (state == FieldState.WhiteKing || state == FieldState.WhitePiece) {
+        return Player.White;
+    }
+    else if (state == FieldState.BlackPiece) {
+        return Player.Black;
+    }
+    else {
+        return null;
+    }
+}
+exports.player = player;
+var Player;
+(function (Player) {
+    Player["White"] = "White";
+    Player["Black"] = "Black";
+})(Player = exports.Player || (exports.Player = {}));
+var BoardSide;
+(function (BoardSide) {
+    BoardSide["White"] = "White";
+    BoardSide["Black"] = "Black";
+    BoardSide["Spectator"] = "Spectator";
+})(BoardSide = exports.BoardSide || (exports.BoardSide = {}));
---
+++ tafli/src/ts/game.js
@@ -0,0 +1,21 @@
+"use strict";
+exports.__esModule = true;
+exports.PreviousMoveInfo = exports.PlayerInfo = exports.Game = void 0;
+var Game = /** @class */ (function () {
+    function Game() {
+    }
+    return Game;
+}());
+exports.Game = Game;
+var PlayerInfo = /** @class */ (function () {
+    function PlayerInfo() {
+    }
+    return PlayerInfo;
+}());
+exports.PlayerInfo = PlayerInfo;
+var PreviousMoveInfo = /** @class */ (function () {
+    function PreviousMoveInfo() {
+    }
+    return PreviousMoveInfo;
+}());
+exports.PreviousMoveInfo = PreviousMoveInfo;
